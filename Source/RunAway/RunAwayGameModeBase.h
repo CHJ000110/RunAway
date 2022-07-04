d88735b5518d3bb8f7f056ba0fcb94d37f4183ab
CHJ000110 <chj000110@kakao.com>
2022-07-04 22:58:10 +0900

    첫 커밋

---
+++ Source/RunAway/RunAwayGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RunAwayGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNAWAY_API ARunAwayGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
